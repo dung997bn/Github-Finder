09291329b408e4c78f775766fb05bfdf8d61aad5
DUNG <dung@gmail.com>
2020-01-01 19:37:29 +0700

    Lesson 10

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
-import { render } from 'react-dom';
+import Navbar from './components/layout/Navbar'
+import Users from './components/users/Users'
 
 class App extends Component {
   render() {
-    const name = 'Kirin'
-    const loading = false
-
     return (
       <div className="App">
-        <h1>My App</h1>
-        {loading ? (<h4>Loading...</h4>) : (<h1>Hello {name}</h1>)}
-        <div>Hello</div>
+        <Navbar />
+        <div className="container">
+          <Users />
+        </div>
       </div>
-
     );
   }
 }

efe135589809cb9d30fe996cc2244003f57c1463
dungnx <dungnx@newwave.vn>
2020-01-02 10:26:37 +0700

    Lesson 14

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import React, { Component } from 'react';
-import './App.css';
-import Navbar from './components/layout/Navbar'
-import Users from './components/users/Users'
-import axios from 'axios'
+import React, { Component } from "react";
+import "./App.css";
+import Navbar from "./components/layout/Navbar";
+import Users from "./components/users/Users";
+import axios from "axios";
 
 class App extends Component {
   state = {
     users: [],
     loading: false
-  }
+  };
 
   async componentDidMount() {
     this.setState({ loading: true });
-    const res = await axios.get('https://api.github.com/users');
-    this.setState({ users: res.data, loading: false })
+    const res = await axios.get(
+      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    this.setState({ users: res.data, loading: false });
   }
 
   render() {
